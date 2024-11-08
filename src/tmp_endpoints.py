da78f630f76da95bb391811ea65c3cb66b4daeb8
ptorras <ptorras@cvc.uab.cat>
2024-11-08 21:50:01 +0000

    remove tmp endpoints

--- src/tmp_endpoints.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-GLOBAL_SBERT_CAT = SentenceTransformer("projecte-aina/ST-NLI-ca_paraphrase-multilingual-mpnet-base")
-def produce_sbert_embedding_cat(query):
-    return  GLOBAL_SBERT_CAT.encode(query).tolist()
-
-
