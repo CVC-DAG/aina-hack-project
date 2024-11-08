d8aee5852b752990d24ac13c0860bd453f4f1b65
EauDeData <amolina@cvc.uab.cat>
2024-11-08 22:51:18 +0100

    putaaaa

--- core.py
+++ core.py
@@ -5,14 +5,31 @@ from src.common import *
 from flask import Flask
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 scimatcher = SciMatcher(SCIMATCHER_PATH)
 record_empreses = pd.read_csv(EMPRESES_RECORD, names=['url', 'vdb', 'metadata'])
-empreses = [Empresa(url, None, None, vdb, meta) for (url, vdb, meta) in []]
+print(record_empreses)
 
-@app.route("/")
+empreses = [Empresa(url, None, None, vdb, meta) for (url, vdb, meta) in zip(record_empreses['url'],
+                                                                            record_empreses['vdb'],
+                                                                            record_empreses['metadata'])]
+print(empreses)
+@app.route("/upload_business/")
 def hello_world():
     return "<p>Hello, World!</p>"
+def upload_business(business_url):
+    vdb_path = business_url.split('.')[0] + '.ann'
+    metapath = business_url.split('.')[0] + '.json'
+    empresa_nova = Empresa(business_url, None, 10, vdbpath=vdb_path, metadata_path=metapath)
 
-def upload_business():
-    return None
+    with open(EMPRESES_RECORD, 'a+') as handler:
+        handler.write(','.join([business_url, vdb_path, empresa_nova]))
+
+    empreses.append(empresa_nova)
+
+    return empreses[-1]
+
+
+
+
+
--- src/common.py
+++ src/common.py
@@ -1,2 +1,5 @@
 SCIMATCHER_PATH = 'data/scimatcher.json'
 EMPRESES_RECORD = 'data/empreses.csv'
+
+TEMPLATES = './templates/htmls/'
+STATIC = './templates/static/'
