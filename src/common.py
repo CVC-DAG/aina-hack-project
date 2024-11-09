c62e1525541a965f5329ecea4a6e0d765c8595a6
Carlos Boned <cbonedriera@gmail.com>
2024-11-09 03:45:27 +0100

    graph retrieval done

--- src/common.py
+++ src/common.py
@@ -3,3 +3,13 @@ EMPRESES_RECORD = 'data/empreses.csv'
 
 TEMPLATES = './templates/htmls/'
 STATIC = './templates/static/'
+
+LOOK_UP_TABLE_ABSTRACTS = "data/look_up_table_abstracts.pkl"
+LOOK_UP_TABLE_KEY_WORDS = "data/look_up_table_key_words.pkl"
+LOOK_UP_TABLE_TITLE = "data/look_up_table_title.pkl"
+
+EMBEDDINGS_ABSTRACTS = "data/abstract.vdb"
+EMBEDDINGS_KEY_WORDS = "data/key_words.vdb"
+EMBEDDINGS_TITLE = "data/titles.vdb"
+
+GRAPH_PATH = "data/graph.graphml" 
