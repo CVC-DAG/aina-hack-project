2d420a2c8311a8f11bd5e7b40e6f6321205c2a45
EauDeData <amolina@cvc.uab.cat>
2024-11-09 06:01:31 +0100

    i el boton se fue a la puta

--- src/common.py
+++ src/common.py
@@ -4,14 +4,14 @@ EMPRESES_RECORD = 'data/empreses.csv'
 TEMPLATES = './templates/htmls/'
 STATIC = './templates/static/'
 
-LOOK_UP_TABLE_ABSTRACTS = "data/look_up_table_abstracts.pkl"
-LOOK_UP_TABLE_KEY_WORDS = "data/look_up_table_key_words.pkl"
-LOOK_UP_TABLE_TITLE = "data/look_up_table_title.pkl"
+LOOK_UP_TABLE_ABSTRACTS = "data/look_up_table_abstracts_small.pkl"
+LOOK_UP_TABLE_KEY_WORDS = "data/look_up_table_key_words_small.pkl"
+LOOK_UP_TABLE_TITLE = "data/look_up_table_title_small.pkl"
 
-EMBEDDINGS_ABSTRACTS = "data/abstract.vdb"
-EMBEDDINGS_KEY_WORDS = "data/key_words.vdb"
-EMBEDDINGS_TITLE = "data/titles.vdb"
+EMBEDDINGS_ABSTRACTS = "data/abstract_small.vdb"
+EMBEDDINGS_KEY_WORDS = "data/key_words_small.vdb"
+EMBEDDINGS_TITLE = "data/titles_small.vdb"
 
-GRAPH_PATH = "data/graph.graphml" 
+GRAPH_PATH = "data/graph_small.graphml"
 
 GRAPH_NX_WEB = "https://ouestware.gitlab.io/retina/beta/#/graph/?url=https://gist.githubusercontent.com/EauDeData/d101bfdbcca2d52afb27864786888e2a/raw/86593990144e0fd963d6022cea666ca231842a0d/network-37f29879-d6b.gexf"
