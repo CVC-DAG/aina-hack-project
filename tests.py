3ccb2b1a98e7ee3f68479f2fac498b4ec05aa938
Artemis <allabres@cvc.uab.cat>
2024-11-09 07:29:30 +0100

    asjgk

--- tests.py
+++ tests.py
@@ -4,14 +4,15 @@ from src.ciencia.scimatcher import SciMatcher
 from src.llm.get_answers import get_answers
 from src.common import *
 
-empresa = Empresa('https://www.allread.ai/ca/', None, 20, vdbpath='data/allread.vdb', metadata_path='data/allread.json')
+#empresa = Empresa('https://www.allread.ai/ca/', None, 20, vdbpath='data/allread.vdb', metadata_path='data/allread.json')
+empresa = Empresa('https://www.lafoneria.com/ca/', None, 20, vdbpath='data/foneria.vdb', metadata_path='data/foneria.json')
 scimatcher = SciMatcher(path_abstract=(EMBEDDINGS_ABSTRACTS, LOOK_UP_TABLE_ABSTRACTS),
                         path_title=(EMBEDDINGS_TITLE, LOOK_UP_TABLE_TITLE),
                         path_key_words=(EMBEDDINGS_KEY_WORDS, LOOK_UP_TABLE_KEY_WORDS),
                         path_graph=GRAPH_PATH,
                         scimatcher_db=SCIMATCHER_PATH)
 
-nom_convocatoria = 'DiH4CAT'
+nom_convocatoria = 'cupons' #'DiH4CAT'
 
 save_path = f'{nom_convocatoria}_answers.json'
 answers = get_answers(empresa, scimatcher, nom_convocatoria)
