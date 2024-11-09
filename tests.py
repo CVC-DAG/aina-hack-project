27c141530e07b06f86a0864cb72ed59b5ef1bf66
Artemis <allabres@cvc.uab.cat>
2024-11-09 07:53:27 +0100

    tests

--- tests.py
+++ tests.py
@@ -4,8 +4,8 @@ from src.ciencia.scimatcher import SciMatcher
 from src.llm.get_answers import get_answers
 from src.common import *
 
-#empresa = Empresa('https://www.allread.ai/ca/', None, 20, vdbpath='data/allread.vdb', metadata_path='data/allread.json')
-empresa = Empresa('https://www.lafoneria.com/ca/', None, 20, vdbpath='data/foneria.vdb', metadata_path='data/foneria.json')
+empresa = Empresa('https://www.allread.ai/ca/', None, 20, vdbpath='data/allread.vdb', metadata_path='data/allread.json')
+#empresa = Empresa('https://www.lafoneria.com/ca/', None, 20, vdbpath='data/foneria.vdb', metadata_path='data/foneria.json')
 scimatcher = SciMatcher(path_abstract=(EMBEDDINGS_ABSTRACTS, LOOK_UP_TABLE_ABSTRACTS),
                         path_title=(EMBEDDINGS_TITLE, LOOK_UP_TABLE_TITLE),
                         path_key_words=(EMBEDDINGS_KEY_WORDS, LOOK_UP_TABLE_KEY_WORDS),
